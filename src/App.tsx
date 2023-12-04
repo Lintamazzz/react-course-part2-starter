1d7ffbcddaad8224225972f483d9fe69851baaba
Lintamazzz <lintama@foxmail.com>
2023-12-04 21:02:53 +0800

    Exercise: working with reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Counter from "./state-management/Counter";
+import LoginStatus from "./state-management/LoginStatus";
 import TaskList from "./state-management/TaskList";
 
 function App() {
   return (
     <>
-      <TaskList />
+      <LoginStatus />
     </>
   );
 }
