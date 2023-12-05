a2023656abba56978992a4666963ca0b9c4f8f98
Lintamazzz <lintama@foxmail.com>
2023-12-05 17:07:58 +0800

    Getting data about the current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,15 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  const [searchParams] = useSearchParams();
+  const location = useLocation();
+
+  // console.log("path params: ", params);    // {id:3}
+  // console.log("search params: ", searchParams.toString());  // name=Alice&age=25
+  // console.log("name: ", searchParams.get("name"));         // Alice
+  // console.log("location: ", location)
+
   return <p>User</p>;
 };
 
