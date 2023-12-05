54c0c5b08503d7f189aa1e9c5ad3affb7eff4b0b
Lintamazzz <lintama@foxmail.com>
2023-12-05 11:13:27 +0800

    Exercise: organizing code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
-import AuthProvider from "./state-management/AuthProvider";
+import AuthProvider from "./state-management/auth/AuthProvider";
 import { TasksProvider } from "./state-management/tasks";
 
 function App() {
